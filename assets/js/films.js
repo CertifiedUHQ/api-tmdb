ed7ddc1c7fb17de4a3f65703a59a9d5613c433e7
CertifiedUHQ <auxence.payen@proton.me>
2024-12-18 21:21:31 +0100

    Update films.js

--- assets/js/films.js
+++ assets/js/films.js
@@ -8,15 +8,12 @@ let left = document.querySelector("#arrow-left");
 let rightSerie = document.querySelector("#arrow-right-series");
 let leftSerie = document.querySelector("#arrow-left-series");
 let popularListContainer = document.querySelector(".popular-list-container");
-let popularListContainerSeries = document.querySelector(
-  ".popular-list-container-series"
-);
+let popularListContainerSeries = document.querySelector(".popular-list-container-series");
 
 let searchImg = document.querySelector(".search img");
 let searchInput = document.querySelector(".search input");
 let url = "https://api.themoviedb.org/3/movie/popular?language=fr-FR&page=1";
-let urlSeries =
-  "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200";
+let urlSeries ="https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=200";
 
 window.addEventListener("DOMContentLoaded", () => {
   refreshImg();
@@ -100,70 +97,3 @@ function refreshImg() {
     })
     .catch((err) => console.error("Erreur:", err));
 }
-
-
-async function searchMovies(query) {
-  const options = {
-      method: "GET",
-      headers: {
-          accept: "application/json",
-          Authorization: `Bearer ${API_KEY}`
-      }
-  };
-  let urlQuery = `https://api.themoviedb.org/3/search/multi?query=${encodeURIComponent(query)}&include_adult=false&language=en-US&page=1`;
-      let response = await fetch(urlQuery, options);
-      let data = await response.json();
-
-      let popularList = document.querySelector(".popular-list-search");
-      popularList.innerHTML = "";
-
-      if (data.results && data.results.length > 0) {
-          data.results.slice(0, 20).forEach((movie) => {
-              if (!movie.poster_path || movie.vote_average === null) {
-                  return; 
-              }
-
-              let container = document.createElement("div");
-              let imageDiv = document.createElement("div");
-              let movieDiv = document.createElement("div");
-              let titleRatingDiv = document.createElement("div");
-              let movieTitle = document.createElement("h3");
-              let movieStar = document.createElement("span");
-              
-              
-              container.className = "movie-container-search";
-              imageDiv.className = "card-img-search";
-              imageDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/w500${movie.poster_path})`;
-              movieDiv.className = "card-content-search";
-              titleRatingDiv.className = "title-rating-search";
-              movieTitle.textContent = movie.title || movie.name;
-              movieStar.textContent = `⭐ ${movie.vote_average.toFixed(1)}/10`;
-
-              titleRatingDiv.appendChild(movieTitle);
-              titleRatingDiv.appendChild(movieStar);
-              movieDiv.appendChild(titleRatingDiv);
-              container.appendChild(imageDiv);
-              container.appendChild(movieDiv);
-
-              popularList.appendChild(container);
-          });
-      }
-}
-
-searchInput.addEventListener("keyup", function() {
-      let searchInput = document.querySelector("#search-input");
-      let query = searchInput.value.trim();
-      let popularList = document.querySelector(".div-container-search");
-      if (query) {
-          leftSearch.style.display = "flex";
-          rightSearch.style.display = "flex";
-          popularList.style.display = "flex";
-          setTimeout(() => { 
-              searchMovies(query);
-          }, 250);
-      } else {
-          leftSearch.style.display = "none";
-          rightSearch.style.display = "none";
-          popularList.style.display = "none";
-      }
-});
